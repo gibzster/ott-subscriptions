89e3923b22d11367a4e8bdc69dbff5f8a78e38ff
Gabby <igibzster@gmail.com>
2019-02-11 03:40:54 -0800

    partial basic page layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+include "json"
+file = File.open "sample_data.json"
+data = JSON.parse(file)

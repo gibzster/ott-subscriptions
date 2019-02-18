47d175fee72de52b949ada8a61fbe8cdbc252252
Gabby <igibzster@gmail.com>
2019-02-18 02:51:25 -0800

    desktop and mobile designs complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     if params[:query]
-         @subscriptions = Subscription.joins(:customer).joins(:product).where('email LIKE ? OR name LIKE ? COLLATE utf8_general_ci', "%#{params[:query]}%","%#{params[:query]}%").page(params[:page])
+         @subscriptions = Subscription.joins(:customer).joins(:product).where('email LIKE ? OR name LIKE ? COLLATE utf8_general_ci', "%#{params[:query]}%","%#{params[:query]}%").order('subscriptions.subscribed_at DESC').page(params[:page])
     else
          @subscriptions = Subscription.order('subscriptions.subscribed_at DESC').page(params[:page])
     #     @subscription = Subscription.new
